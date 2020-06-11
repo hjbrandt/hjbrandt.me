1fb4be74168eea2582fa0e6b6a67175b5535be6e
Hans-Jakob Brandt <34381881+hjbrandt@users.noreply.github.com>
2020-06-11 16:12:07 +0200

    fix: pageWrapper import path

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { NextPage } from 'next'
 // Components
 import { Header } from '../components/layout/header/Header'
 import { Nav } from '../components/layout/navigation/Nav'
-import { PageWrapper } from '../components/layout/pageWrapper/PageWrapper.tsx'
+import { PageWrapper } from '../components/layout/pageWrapper/PageWrapper'
 import { Hero } from '../components/hero/Hero.tsx'
 
 interface Props {
