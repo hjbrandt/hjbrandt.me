92371ec6eb23e1e7d67532094fcb7e7cd7319777
Hans-Jakob Brandt <34381881+hjbrandt@users.noreply.github.com>
2020-06-17 15:17:54 +0200

    build: Add styled components

--- atoms/logo/Logo.tsx
+++ atoms/logo/Logo.tsx
@@ -1,5 +1,19 @@
 import React from 'react'
 import Link from 'next/link'
+import styled from 'styled-components'
+
+const LogoH1 = styled.h1`
+  width: 1px !important;
+  height: 1px !important;
+  padding: 0px !important;
+  margin: -1px !important;
+  white-space: nowrap !important;
+  position: absolute !important;
+  overflow: hidden !important;
+  border: 0px none !important;
+  clip: rect(1px, 1px, 1px, 1px) !important;
+  clip-path: inset(50%) !important;
+`
 
 import styles from './Logo.module.css'
 
@@ -7,9 +21,7 @@ export function Logo() {
   return (
     <Link href="/">
       <a>
-        <h1 className={styles.logoHeading}>
-          Personal website of Hans-Jakob Brandt
-        </h1>
+        <LogoH1>Personal website of Hans-Jakob Brandt</LogoH1>
         <img
           className={styles.logo}
           src="/Logo.png"
