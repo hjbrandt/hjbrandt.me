89fd55741cb597baa934b1ddf9efc5c9fe4a3388
Hans-Jakob Brandt <34381881+hjbrandt@users.noreply.github.com>
2020-06-17 14:10:28 +0200

    chore: Clean up some styling

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 
 import styles from './Hero.module.css'
-const { right, left } = styles
+const { hero, right, left } = styles
 
 export const Hero = () => {
   return (
-    <h3>
-      <span className={right}>I&apos;m an</span>
-      <span className={left}>interactive</span>
-      <span className={right}>developer</span>
-      <span className={left}>based</span>
-      <span className={right}>in Oslo.</span>
-    </h3>
+    <div className={hero}>
+      <h3>
+        <span className={right}>I&apos;m an</span>
+        <span className={left}>interactive</span>
+        <span className={right}>developer</span>
+        <span className={left}>based</span>
+        <span className={right}>in Oslo.</span>
+      </h3>
+    </div>
   )
 }
--- components/layout/navigation/Nav.tsx
+++ components/layout/navigation/Nav.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Link from 'next/link'
 
 import styles from './Nav.module.css'
+const { navMenu, navLink } = styles
 
 export const Nav = () => {
-  const { navMenu, navLink } = styles
   return (
     <nav className={navMenu} aria-label="Main Navigation">
       <Link href="/contact">
