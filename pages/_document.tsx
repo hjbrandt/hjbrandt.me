455a023da499490522fe19d2f9d32093823ffb5b
Hans-Jakob Brandt <34381881+hjbrandt@users.noreply.github.com>
2020-06-17 13:48:47 +0200

    feat: Import adobe fonts

---
+++ pages/_document.tsx
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://cdn.rawgit.com/jgthms/minireset.css/master/minireset.min.css"
+          />
+          <link rel="stylesheet" href="https://use.typekit.net/hmi2dah.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
