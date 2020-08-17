ef4a2371912882a1cf5f5eaa7b82372a0828a4f1
Hans-Jakob Brandt <34381881+hjbrandt@users.noreply.github.com>
2020-08-17 12:40:38 +0200

    feature: Add Announcement component te header

--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -3,10 +3,11 @@ import React from 'react'
 import styles from './Header.module.css'
 
 import { Logo } from '../../../atoms/logo/Logo'
-
+import { Announcement } from '../../../atoms/announcement/Announcement.tsx'
 export const Header = () => {
   return (
     <header className={styles.header}>
+      <Announcement />
       <Logo />
     </header>
   )
