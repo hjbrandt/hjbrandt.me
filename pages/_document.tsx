ba02f34700998e26700fcb5c8b9845c95949141f
Hans-Jakob Brandt <34381881+hjbrandt@users.noreply.github.com>
2021-04-11 17:05:28 +0200

    chore: upgrade nextjs

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,32 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const styleSheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: App => props =>
-            styleSheet.collectStyles(<App {...props} />),
-        })
-
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {styleSheet.getStyleElement()}
-          </>
-        ),
-      }
-    } finally {
-      styleSheet.seal()
-    }
-  }
 
   render() {
     return (
