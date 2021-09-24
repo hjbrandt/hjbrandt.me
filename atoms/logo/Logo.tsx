cc10bf30c32f0af13239e626c9dbfeaa0ace5546
Hans-Jakob Brandt <34381881+hjbrandt@users.noreply.github.com>
2021-09-24 14:23:05 +0200

    use next image

--- atoms/logo/Logo.tsx
+++ atoms/logo/Logo.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 import styles from './Logo.module.css'
 
@@ -8,7 +9,7 @@ export function Logo() {
     <Link href="/">
       <a>
         <h1 className={styles.logoHeading}>Personal website of Hans-Jakob Brandt</h1>
-        <img
+        <Image
           className={styles.logo}
           src="/Logo.png"
           alt="Hans-Jakob Brandt Logo"
