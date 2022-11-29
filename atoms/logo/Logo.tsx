36e550b3a00b1792b4244fe61cf88dd1df643296
Hans-Jakob Brandt <34381881+hjbrandt@users.noreply.github.com>
2022-11-29 15:09:27 +0100

    Update Logo.tsx

--- atoms/logo/Logo.tsx
+++ atoms/logo/Logo.tsx
@@ -7,15 +7,18 @@ import styles from './Logo.module.css'
 export function Logo() {
   return (
     <Link href="/">
-      <h1 className={styles.logoHeading}>
-        Personal website of Hans-Jakob Brandt
-      </h1>
-      <Image
-        className={styles.logo}
-        layout="fill"
-        src="/Logo.png"
-        alt="Hans-Jakob Brandt Logo"
-      />
+      <>
+        <h1 className={styles.logoHeading}>
+          Personal website of Hans-Jakob Brandt
+        </h1>
+        <Image
+          className={styles.logo}
+          width={500}
+          height={500}
+          src="/Logo.png"
+          alt="Hans-Jakob Brandt Logo"
+        />
+      </>
     </Link>
   )
 }
